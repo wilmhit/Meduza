30ff0130e488a31a9b34f65ef668e5ec0886b047
Lewy <lewy12121212@gmail.com>
2021-03-16 15:52:31 +0100

    Adding test programs to the first VoIP connection

--- client/__main__.py
+++ client/__main__.py
@@ -2,9 +2,13 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from views import MainWindow
+import socket
+import logging
+import threading
+import time
 
 
 if __name__ == "__main__":
     window = MainWindow()
     window.show_all()
-    Gtk.main()
+    Gtk.main() # do osobnego threda
