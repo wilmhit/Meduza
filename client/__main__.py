04ec5ce9254b932021e65fa8c432ba91b61629c2
Jcob Chodubski <jchodubski@pm.me>
2021-04-09 18:22:01 +0200

    Added client ping

--- client/__main__.py
+++ client/__main__.py
@@ -3,11 +3,10 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from views import MainWindow
 from threading import Thread
-from audio import connect
 
 if __name__ == "__main__":
     window = MainWindow()
     window.show_all()
     ui_thread = Thread(target=Gtk.main)
-    connect()
+    ui_thread.start()
     ui_thread.join()
--- client/audio.py
+++ client/audio.py
@@ -3,9 +3,12 @@ import socket
 # This is basic draft
 # Make this work for now
 # It doesn't need to change channels or talk to gui as of now
+SERVER_ADDRESS = ("1.123.12.2", )
+
 
 def connect():
     connection = ConnectionManager()
+    connection.connect_channel(1)
     # connection.connect_channel(user_selected_channel)
     # use pyAudio to open recording and listening streams
     # while True:
@@ -14,14 +17,24 @@ def connect():
 
 
 class ConnectionManager():
-
-    def __init__():
+    def __init__(self, server_address, server_port):
+        self.server_address = server_address
+        self.server_port = server_port
+        self.metadata_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         # Open 2 random ports: 1. Metadata port 2. Audio port
         # Check here connection to server metadata port using
         # client metadata port
         pass
 
-    def connect_channel(channel):
+    def ping(self, ip, port):
+        message = b"PING"
+        self.metadata_socket.sendto(message, ip, port)
+        returned, server = self.metadata_socket.recvfrom(4)
+        return returned == b"PONG"
+
+    def connect_channel(self, channel):
+        message = b"Hey, i want to connent to channel: " + channel
+        self.metadata_socket.sendto(message, self.server_address, self.server_port)
         ## ESTABLISHING AUDIO PORT
         # Send request from client metadata port to server metadata port
         # In request include channel you want to connect to
@@ -31,15 +44,12 @@ class ConnectionManager():
         # Why? Server doesn't know what is client's audio port. You can't
         # just send port number. NAT might change it without client knowing
         # about it. Check wikipedia: UDP hole punching.
-        pass
-    
-    def recv_audio_packet():
+
+    def recv_audio_packet(self):
         # This method reads client audio port buffer
         # Will raise an error if used before connect_channel(channel)
         pass
 
-    def send_audio_packet():
+    def send_audio_packet(self):
         # Same as above
         pass
-
-
