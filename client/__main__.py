6954dd2bd40be259295799b9244c2ca1489c3ca8
Lewy <lewy12121212@gmail.com>
2021-04-09 18:20:48 +0200

    Add basic server draft ( Ping -> Pong )

--- client/__main__.py
+++ client/__main__.py
@@ -3,11 +3,13 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from views import MainWindow
 from threading import Thread
-from audio import connect
+#from audio import connect
 
 if __name__ == "__main__":
     window = MainWindow()
     window.show_all()
     ui_thread = Thread(target=Gtk.main)
-    connect()
+    #connect()
+    ui_thread.start()
     ui_thread.join()
+    
