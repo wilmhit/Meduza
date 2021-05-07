f5e628c58b6c1a204f1c4aae707fe93afba38c0e
Jcob Chodubski <jchodubski@pm.me>
2021-05-07 20:05:16 +0200

    Gui update (#2)
    * Added signal class
    
    * Updated ConnectionManager to use Signal class
    
    * Added password modal
    
    * Added password field to modal
    
    * Change Radio buttons to checkboxes
    
    * Updated gui

---
+++ client/gui.py
@@ -0,0 +1,9 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from .views import MainWindow, PasswordModal
+
+def run_gui():
+    window = MainWindow()
+    window.show_all()
+    Gtk.main()
--- client/gui_callbacks.py
+++ client/gui_callbacks.py
@@ -1,21 +1,70 @@
+import time
 
-def disconnect_button(button):
-    print("Input received!")
+S_BETWEEN_CLICKS = 0.005
+CHANNELS = 5
 
-def boom_button(button):
-    print("Input received!")
+gui_state = {
+    "muted_mic": False,
+    "muted_spk": False,
+    "last_click": 0.0,
+    "connected": False,
+    "channels": []
+}
 
-def channel_0_button(button):
-    print("Input received!")
+def create_channel(id):
+    return {
+        "display": "Channel " + str(id),
+        "connected": False
+    }
 
-def mute_all_checkbox(button):
-    print("Input received!")
+for x in range(CHANNELS):
+    gui_state["channels"].append(create_channel(x))
 
-def mute_mic_checkbox(button):
-    print("Input received!")
 
-def channel_callback():
-    print("Input received!")
+def is_channel_connected(channel_id):
+    return gui_state["channels"][channel_id]["connected"]
 
-def get_channels():
-    return ["Channel 1", "Channel 2"]
+def is_connected_to_any_channel():
+    for channel in gui_state["channels"]:
+        if channel["connected"]:
+            return True
+    return False
+
+def is_connected():
+    return gui_state["connected"] 
+
+def disconnect():
+    gui_state["connected"] = False
+    for channel in gui_state["channels"]:
+        channel["connected"] = False
+
+def time_lock():
+    current_time = time.thread_time()
+    last_time = gui_state["last_click"]
+    time_passed = current_time - last_time
+    gui_state["last_click"] = current_time
+    # is negative for first click
+    return time_passed > S_BETWEEN_CLICKS or time_passed < 0
+
+def connect_channel(channel_id):
+    for channel in gui_state["channels"]:
+        channel["connected"] = False
+    gui_state["channels"][channel_id]["connected"] = True
+
+def connect_with_password(channel_id, password): 
+    connect_channel(channel_id)
+
+def is_protected_channel(channel_id):
+    return channel_id == 0
+
+def boom_callback():
+    print("boom")
+
+def mute_mic(_):
+    pass
+
+def connect_to_server(address):
+    gui_state["connected"] = True
+
+def mute_spk(_):
+    pass
