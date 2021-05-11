625fb0555385f6f50de239d0dbc48c185d81223e
Jcob Chodubski <jchodubski@pm.me>
2021-05-11 14:20:10 +0200

    Added server mock_main

--- server/__init__.py
+++ server/__init__.py
@@ -7,9 +7,14 @@ from .voip_server import Server
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 50001
+PASSWORD = "pass"
+NUM_OF_CHANNELS = 10
 
 def main():
-    server = Server(UDP_IP, UDP_PORT, "passs", 10)
+    server = Server(UDP_IP, UDP_PORT, PASSWORD, NUM_OF_CHANNELS)
     server.run()
     #rec_thr = Thread(target=Server.listen)
     #rec_thr.start()
+
+def mock_main():
+    server = Server(UDP_IP, UDP_PORT, PASSWORD, NUM_OF_CHANNELS)
